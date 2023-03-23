400666a64647e7a4a81386947d4a9b5679e7ef9f
JDogM <matt.mll@hotmail.com>
2023-03-23 11:56:41 +0100

    changes

--- main.py
+++ main.py
@@ -1,59 +1,60 @@
 import PyPDF3 as pdf
 import os
 
-#custom startpage implementing
+for i in range(5, 7):
+	#custom startpage implementing
+	name = "BioMEMS_ch" + str(i)
+	input = pdf.PdfFileReader(open("/Users/matthiasmonden/Downloads/" + name + ".pdf", "rb"))
+	output = pdf.PdfFileWriter()
 
-input = pdf.PdfFileReader(open("course.pdf", "rb"))
-output = pdf.PdfFileWriter()
+	pages = []
+	currentPage = -1
 
-pages = []
-currentPage = -1
+	x1_A4 = 612
+	y1_A4 = 792
 
-x1_A4 = 612
-y1_A4 = 792
+	std_offset_x = 30
+	std_offset_y = 17
+	padding = 25
 
-std_offset_x = 30
-std_offset_y = 17
-padding = 25
+	offsetX = x1_A4 - std_offset_x
+	offsetY = y1_A4 - std_offset_y
 
-offsetX = x1_A4 - std_offset_x
-offsetY = y1_A4 - std_offset_y
+	def getPageSize(page):
+		x0 = page['/MediaBox'][0]
+		y0 = page['/MediaBox'][1]
+		x1 = page['/MediaBox'][2]
+		y1 = page['/MediaBox'][3]
 
-def getPageSize(page):
-	x0 = page['/MediaBox'][0]
-	y0 = page['/MediaBox'][1]
-	x1 = page['/MediaBox'][2]
-	y1 = page['/MediaBox'][3]
+		return x0, y0, x1, y1
 
-	return x0, y0, x1, y1
+	def getScaling(pdf_input, pagenumber):
+		inputPage = pdf_input.getPage(pageNumber)
 
-def getScaling(pdf_input, pagenumber):
-	inputPage = pdf_input.getPage(pageNumber)
+		x0, y0, x1, y1 = getPageSize(inputPage)
 
-	x0, y0, x1, y1 = getPageSize(inputPage)
+		scaling_factor = (x1_A4 - 2*std_offset_x)/(3*y1 + 2*padding)
+		x1_scaled = x1*scaling_factor
+		y1_scaled = y1*scaling_factor
 
-	scaling_factor = (x1_A4 - 2*std_offset_x)/(3*y1 + 2*padding)
-	x1_scaled = x1*scaling_factor
-	y1_scaled = y1*scaling_factor
+		return scaling_factor, x1_scaled, y1_scaled
 
-	return scaling_factor, x1_scaled, y1_scaled
+	for pageNumber in range(input.getNumPages()):
+		if pageNumber%6 == 0:
+			pages.append(pdf.PdfFileWriter().addBlankPage(x1_A4, y1_A4))
 
-for pageNumber in range(input.getNumPages()):
-	if pageNumber%6 == 0:
-		pages.append(pdf.PdfFileWriter().addBlankPage(x1_A4, y1_A4))
+			currentPage += 1
+			secondRow = 0
 
-		currentPage += 1
-		secondRow = 0
+		inputPage = input.getPage(pageNumber)
+		scaling_factor, x1_scaled, y1_scaled = getScaling(input, pageNumber)
 
-	inputPage = input.getPage(pageNumber)
-	scaling_factor, x1_scaled, y1_scaled = getScaling(input, pageNumber)
+		if pageNumber%3 == 0 and pageNumber != 0 and pageNumber%6 != 0:
+			secondRow = y1_A4/2
 
-	if pageNumber%3 == 0 and pageNumber != 0 and pageNumber%6 != 0:
-		secondRow = y1_A4/2
+		pages[currentPage].mergeRotatedScaledTranslatedPage(inputPage, -90, scaling_factor, offsetX - (pageNumber%3 + 1)*y1_scaled - (pageNumber%3)*(padding-10) + 5, offsetY - secondRow)
 
-	pages[currentPage].mergeRotatedScaledTranslatedPage(inputPage, -90, scaling_factor, offsetX - (pageNumber%3 + 1)*y1_scaled - (pageNumber%3)*(padding-10) + 5, offsetY - secondRow)
+	for p in pages:
+		output.addPage(p.rotateCounterClockwise(90))
 
-for p in pages:
-	output.addPage(p.rotateCounterClockwise(90))
-
-output.write(open("MPT_SlidesPER6.pdf", "wb"))
+	output.write(open("/Users/matthiasmonden/Downloads/MEMS_Slides-" + str(i) + "_per6.pdf", "wb"))
